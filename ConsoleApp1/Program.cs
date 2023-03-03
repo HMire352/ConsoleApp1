d6f2139c85d822dc5b002ede781e1157e9e3d50f
Elev <Elev@TEB125118>
2023-03-03 14:47:38 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace upp1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Add(12, 12, 12);
+            Console.WriteLine(Add(12, 12, 12));
+        }
+
+        static int Add(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+
+    }
+   
+    
+}
